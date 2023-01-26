d7b454056e2ee1cafb893fe00caaa5d1c6945b89
Serhii Baraban <akakieva@gmail.com>
2023-01-26 17:38:33 +0100

    added method to the Vue App

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
       courseGoal: 'Finish the course and learn Vue!',
       vueLink: 'https://vuejs.org/'
     };
+  },
+  methods: {
+    outputGoal(){
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5){
+        return 'Learn Vue!';
+      } else{
+        return 'Master Vue!';
+      }
+    }
   }
 });
 
