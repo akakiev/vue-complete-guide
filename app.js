0b7353a9fc08318e87b773b911434c0ec082c61d
Serhii Baraban <akakieva@gmail.com>
2023-02-01 14:53:24 +0100

    added event binding

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
 const app = Vue.createApp({
   data() {
     return{
-      courseGoalA: 'Finish the course and learn Vue!',
-      courseGoalB: 'Master Vue and build amazing apps!',
-      vueLink: 'https://vuejs.org/'
+      counter: 0,
     };
   },
-  methods: {
-    outputGoal(){
-      const randomNumber = Math.random();
-      if (randomNumber < 0.5){
-        return this.courseGoalA;
-      } else{
-        return this.courseGoalB;
-      }
-    }
-  }
+  
 });
 
-app.mount('#user-goal');
+app.mount('#events');
