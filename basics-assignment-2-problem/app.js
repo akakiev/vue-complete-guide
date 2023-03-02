3bdc67de251b63c6378dd8bfdbe432081bebe189
Serhii Baraban <akakieva@gmail.com>
2023-03-02 13:06:04 +0100

    solved 3) - Repeating 2) but only output the entered value if the ENTER key was pressed

--- basics-assignment-2-problem/app.js
+++ basics-assignment-2-problem/app.js
@@ -2,15 +2,19 @@ const app = Vue.createApp({
     data(){
         return{
             soccerTeam: '',
+            submittedSoccerTeam: '',
         };
     },
     methods: {
+        submittedInput(event){
+            this.submittedSoccerTeam = event.target.value;
+        },
+        confirmedInput(event, club){
+            this.soccerTeam = event.target.value + ' ' + club;
+        },
         pressedButton(event){
             alert('The button is pressed!');
           },
-        confirmedInput(event, club){
-            this.soccerTeam = event.target.value + ' ' + club;
-        }
     }, 
 });
 
