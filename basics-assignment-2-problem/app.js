5735f11cd8aad2543268086954f12daaa09ee176
Serhii Baraban <akakieva@gmail.com>
2023-03-02 12:58:13 +0100

    solved 2) - Registering the user input on "keydown" and output it in the paragraph

--- basics-assignment-2-problem/app.js
+++ basics-assignment-2-problem/app.js
@@ -1,13 +1,16 @@
 const app = Vue.createApp({
     data(){
         return{
-
-        }
+            soccerTeam: '',
+        };
     },
     methods: {
         pressedButton(event){
             alert('The button is pressed!');
           },
+        confirmedInput(event, club){
+            this.soccerTeam = event.target.value + ' ' + club;
+        }
     }, 
 });
 
